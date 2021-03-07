0b327bb6ce570c6c2b654738ad776a42dc2eaf40
Michal <michal.m8833@gmail.com>
2021-03-07 01:16:06 +0100

    recorder fixed

--- src/recorder.c
+++ src/recorder.c
@@ -335,7 +335,7 @@ void timestampFormat(char *buf, struct timespec *ts) {
         perror("localtime_r");
 
     if(registerParameters.globalTimestamp)
-        sprintf(buf, "%d-%02d-%02d %02d:%02d:%02d.%03ld", t.tm_year+1900, t.tm_mon+1, t.tm_yday+1, t.tm_hour, t.tm_min, t.tm_sec, ts->tv_nsec/1000000);
+        sprintf(buf, "%d-%02d-%02d %02d:%02d:%02d.%03ld", t.tm_year+1900, t.tm_mon+1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec, ts->tv_nsec/1000000);
     else
         sprintf(buf, "0:%02d:%02d.%03ld", t.tm_min, t.tm_sec, ts->tv_nsec/1000000);
 }
